fe0902ed991c0f7082932f90edcfc802b0186dd0
Brahim Redouane LEBAD <redouaneleb@gmail.com>
2024-12-06 16:47:57 +0100

    Implemented AI classification with DJL

--- src/main/java/org/example/minidac/ImageClassifier.java
+++ src/main/java/org/example/minidac/ImageClassifier.java
@@ -9,6 +9,7 @@ import ai.djl.repository.zoo.Criteria;
 import ai.djl.repository.zoo.ZooModel;
 
 import java.io.File;
+import java.text.DecimalFormat;
 
 public class ImageClassifier {
     public static String classify(File imageFile) {
@@ -30,9 +31,10 @@ public class ImageClassifier {
 
                 Classifications classifications = predictor.predict(img);
                 System.out.println(classifications.best());
-                double prob = classifications.best().getProbability();
+                double prob = classifications.best().getProbability() * 100;
+                DecimalFormat df = new DecimalFormat("#.####");
                 String name = classifications.best().getClassName().replaceAll("n\\d+", "");
-                return "This image is " + prob * 100 + "%" + " a" + name;
+                return "This image is " + df.format(prob) + "%" + " a" + name;
             }
         } catch (Exception e) {
             return "Error during classification: " + e.getMessage();
